dc7a9567bffe774740cfa27fa0f6bcededbe1045
ahmala123 <ahmadalakash000@gmail.com>
2024-01-12 09:25:48 +0100

    completed

--- 23_026/js/main.js
+++ 23_026/js/main.js
@@ -30,4 +30,31 @@ console.log(Number.MAX_SAFE_INTEGER.toString().length);
 let myVar = "100.56789 Views";
 
 console.log(Number.parseInt(myVar)); // 100
+console.log(+Number.parseFloat(myVar).toFixed(2)); // 100.57
 console.log(Number.parseFloat(myVar).toFixed(2) * 1); // 100.57
+console.log(Number(parseFloat(myVar).toFixed(2))); // 100.57
+
+//Q5
+// num has 10 as value
+// You should use isInteger to get number 2
+//  Don't use any number to get that
+let num = 10;
+
+console.log(Number.isInteger(num) + Number.isInteger(num)); // 2
+
+//Q6
+// use the var flt to get number 10 with 5 diffrent methods
+// you should use only the  value of flt without write any number
+// Don't repeat the methods to get the result
+
+let flt = 10.4;
+
+console.log(flt.toFixed(0) * true); // 10
+console.log(Number.parseInt(flt)); // 10
+console.log(+Number.parseFloat(flt).toFixed(0)); // 10
+console.log(Math.round(flt)); // 10
+console.log(Math.floor(flt)); // 10
+
+//Q7 You schould  write random number between 0 and 4
+
+console.log(Math.floor(Math.random() * 5)); // 0 || 1 || 2 || 3 || 4
